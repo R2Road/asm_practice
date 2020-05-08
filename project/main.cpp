aed5f518ee340f1729ebea1c235bdc8de4037f87
r2road <studior2road@gmail.com>
2020-05-08 09:06:24 +0900

    test

--- project/main.cpp
+++ project/main.cpp
@@ -1,7 +1,16 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+int sum( int left, int right )
+{
+	__asm {
+		mov eax, left;
+		mov ebx, right;
+		add eax, ebx;
+	}
+}
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout << "Sum : " << sum( 1, 2 );
 }
