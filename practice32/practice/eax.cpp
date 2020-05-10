2a895835ad2a84f7e6590c1150061c165cf46683
r2road <studior2road@gmail.com>
2020-05-10 10:11:09 +0900

    summury

--- practice32/practice/eax.cpp
+++ practice32/practice/eax.cpp
@@ -5,6 +5,15 @@ int cpp_eax()
 	return 10;
 }
 
+// Processor Registers
+// > Data Registers
+// > > used for arithmetic, logical, and other operations
+// > > 32 bit
+// > > EAX, EBX, ECX, EDX
+
+// EAX
+// > E is Extended
+// > the AX register corresponds to the lowest 16 bits of the new 32 - bit EAX register
 int asm_eax()
 {
 	__asm mov eax, 10;
